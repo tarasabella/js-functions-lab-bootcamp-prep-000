d20124a23ff12598b39dafc7c1be0faa90ab87a7
Tara E. Sabella <tarasabella@gmail.com>
2018-10-02 21:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(holiday, name) {
+function happyHolidaysTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days) {
